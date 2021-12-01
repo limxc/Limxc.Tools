75be43770e39090fad439055a04e0f4a81aa80d9
limxc <limxc@163.com>
2021-12-01 17:48:12 +0800

    添加LinqExtensions

---
+++ src/Limxc.Tools/Extensions/LinqExtension.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Limxc.Tools.Extensions
+{
+    public static class LinqExtension
+    {
+        public static IEnumerable<IEnumerable<T>> Batch<T>(this IEnumerable<T> source, int size)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (size <= 0) throw new ArgumentOutOfRangeException(nameof(size));
+
+            var batch = new List<T>();
+            foreach (var item in source)
+            {
+                batch.Add(item);
+                if (batch.Count == size)
+                {
+                    yield return batch;
+                    batch = new List<T>();
+                }
+            }
+
+            yield return batch;
+        }
+
+        public static IEnumerable<IEnumerable<T>> Window<T>(this IEnumerable<T> source, int size)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (size <= 0) throw new ArgumentOutOfRangeException(nameof(size));
+
+            return _(); IEnumerable<IEnumerable<T>> _()
+            {
+                using (var iter = source.GetEnumerator())
+                {
+                    var window = new T[size];
+                    int i;
+                    for (i = 0; i < size && iter.MoveNext(); i++)
+                        window[i] = iter.Current;
+
+                    if (i < size)
+                    {
+                        var newWindow = new T[i];
+                        Array.Copy(window, 0, newWindow, 0, i);
+                        yield return newWindow;
+                        yield break;
+                    }
+
+                    while (iter.MoveNext())
+                    {
+                        var newWindow = new T[size];
+                        Array.Copy(window, 1, newWindow, 0, size - 1);
+                        newWindow[size - 1] = iter.Current;
+
+                        yield return window;
+                        window = newWindow;
+                    }
+
+                    yield return window;
+                }
+            }
+        }
+    }
+}
---
+++ tests/Limxc.ToolsTests/Extensions/LinqExtensionTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Limxc.Tools.Extensions;
+using System.Linq;
+using Xunit;
+
+namespace Limxc.ToolsTests.Extensions
+{
+    public class LinqExtensionTests
+    {
+        [Fact]
+        public void BatchTest()
+        {
+            var range = Enumerable.Range(0, 5);
+            range.Batch(3).Should().BeEquivalentTo(new[] { new[] { 0, 1, 2 }, new[] { 3, 4 } });
+            range.Batch(7).Should().BeEquivalentTo(new[] { new[] { 0, 1, 2, 3, 4 } });
+
+            (new int[0]).Batch(3).Should().BeEquivalentTo(new[] { new int[0] });
+        }
+
+        [Fact]
+        public void WindowTest()
+        {
+            var range = Enumerable.Range(0, 5);
+            range.Window(3).Should().BeEquivalentTo(new[] { new[] { 0, 1, 2 }, new[] { 1, 2, 3 }, new[] { 2, 3, 4 } });
+            range.Window(6).Should().BeEquivalentTo(new[] { new[] { 0, 1, 2, 3, 4 } });
+
+            (new int[0]).Window(3).Should().BeEquivalentTo(new[] { new int[0] });
+        }
+    }
+}
