5f89677b8181a9b4ba7c325576b278c98f04f88c
袁文一 <limxc@163.com>
2022-08-24 14:28:53 +0800

    修改名称避免冲突

---
+++ src/Limxc.Tools.Contract/Interfaces/IMessageBusService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Limxc.Tools.Contract.Interfaces
+{
+    public interface IMessageBusService : IDisposable
+    {
+        Task Pub<T>(T payload, string topic = "", CancellationToken token = default);
+        IObservable<T> Sub<T>(string topic = "");
+    }
+}
