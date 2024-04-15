1925fb993dba5e59f8e01f433add879d9f841b86
袁文一 <limxc@163.com>
2024-04-15 23:37:30 +0800

    替换System.Text.Json为Newtonsoft.Json

--- src/Limxc.Tools/Extensions/JsonExtension.cs
+++ src/Limxc.Tools/Extensions/JsonExtension.cs
@@ -1,34 +1,18 @@
-﻿using System.Text.Encodings.Web;
-using System.Text.Json;
-using System.Text.Json.Serialization;
+﻿using Newtonsoft.Json;
 
 namespace Limxc.Tools.Extensions
 {
     public static class JsonExtension
     {
-        public static JsonSerializerOptions Init(this JsonSerializerOptions options, bool readable)
-        {
-            options.Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping;
-            options.WriteIndented = readable;
-            options.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-
-            options.ReadCommentHandling = JsonCommentHandling.Skip;
-            options.AllowTrailingCommas = true;
-
-            //options.NumberHandling = JsonNumberHandling.AllowReadingFromString | JsonNumberHandling.WriteAsString; //6.0.9 有bug
-
-            return options;
-        }
-
         public static string ToJson<T>(this T obj, bool readable = false)
         {
-            var json = JsonSerializer.Serialize(obj, new JsonSerializerOptions().Init(readable));
+            var json = JsonConvert.SerializeObject(obj, readable ? Formatting.Indented : Formatting.None);
             return json;
         }
 
         public static T JsonTo<T>(this string json)
         {
-            return JsonSerializer.Deserialize<T>(json, new JsonSerializerOptions().Init(false));
+            return JsonConvert.DeserializeObject<T>(json);
         }
     }
 }
