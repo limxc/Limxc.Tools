9eb7d1b22bf9d31aa59d248819b5fbf4e53f3379
袁文一 <limxc@163.com>
2024-10-03 15:07:41 +0800

    添加基础数据类型转换ConvertTo

--- src/Limxc.Tools/Extensions/TypeExtension.cs
+++ src/Limxc.Tools/Extensions/TypeExtension.cs
@@ -38,5 +38,89 @@ namespace Limxc.Tools.Extensions
                 return generic == (test.IsGenericType ? test.GetGenericTypeDefinition() : test);
             }
         }
+
+        /// <summary>
+        ///     基础数据类型转换器
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <param name="def"></param>
+        /// <returns></returns>
+        public static T ConvertTo<T>(this object obj, T def)
+        {
+            try
+            {
+                // 如果传入对象是 null 或 DBNull，并且目标类型是 string，返回空字符串
+                if ((obj == null || obj == DBNull.Value) && typeof(T) == typeof(string))
+                    return (T)Convert.ChangeType("", typeof(T));
+
+                // 如果传入对象已经是目标类型，直接返回
+                if (obj is T t) return t;
+
+                // 处理 Nullable<T> 类型
+                if (typeof(T).IsGenericType)
+                {
+                    var genericTypeDefinition = typeof(T).GetGenericTypeDefinition();
+                    if (genericTypeDefinition == typeof(Nullable<>))
+                        // ReSharper disable once AssignNullToNotNullAttribute
+                        return (T)Convert.ChangeType(obj, Nullable.GetUnderlyingType(typeof(T)));
+                }
+
+                // 进行类型转换
+                return (T)Convert.ChangeType(obj, typeof(T));
+            }
+            catch (InvalidCastException)
+            {
+                try
+                {
+                    // 如果发生转换异常，尝试返回原对象的强制转换
+                    return (T)obj;
+                }
+                catch
+                {
+                    return def;
+                }
+            }
+            catch
+            {
+                return def;
+            }
+        }
+
+        /// <summary>
+        ///     基础数据类型转换器
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static T ConvertTo<T>(this object obj)
+        {
+            try
+            {
+                // 如果传入对象是 null 或 DBNull，并且目标类型是 string，返回空字符串
+                if ((obj == null || obj == DBNull.Value) && typeof(T) == typeof(string))
+                    return (T)Convert.ChangeType("", typeof(T));
+
+                // 如果传入对象已经是目标类型，直接返回
+                if (obj is T t) return t;
+
+                // 处理 Nullable<T> 类型
+                if (typeof(T).IsGenericType)
+                {
+                    var genericTypeDefinition = typeof(T).GetGenericTypeDefinition();
+                    if (genericTypeDefinition == typeof(Nullable<>))
+                        // ReSharper disable once AssignNullToNotNullAttribute
+                        return (T)Convert.ChangeType(obj, Nullable.GetUnderlyingType(typeof(T)));
+                }
+
+                // 进行类型转换
+                return (T)Convert.ChangeType(obj, typeof(T));
+            }
+            catch (InvalidCastException)
+            {
+                // 如果发生转换异常，尝试返回原对象的强制转换
+                return (T)obj;
+            }
+        }
     }
 }
--- tests/Limxc.ToolsTests/Extensions/TypeExtensionTests.cs
+++ tests/Limxc.ToolsTests/Extensions/TypeExtensionTests.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System;
+using FluentAssertions;
 using Limxc.Tools.Extensions;
 using Xunit;
 
@@ -11,6 +12,37 @@ public class TypeExtensionTests
     {
         typeof(BirdZoo).IsInheritedFrom(typeof(Zoo<>)).Should().BeTrue();
         typeof(BirdZoo).IsInheritedFrom(typeof(IZoo<>)).Should().BeTrue();
+    }
+
+    [Fact]
+    public void ConvertToTest()
+    {
+        // string->double
+        "12.3".ConvertTo<double>().Should().Be(12.3);
+        "abc".ConvertTo<double>(0).Should().Be(0);
+        var func = () => "abc".ConvertTo<double>();
+        func.Should().Throw<Exception>();
+
+        //double->string
+        12.3.ConvertTo<string>().Should().Be("12.3");
+
+        //class->string 
+        Bird bird = null;
+        // ReSharper disable once ExpressionIsAlwaysNull
+        bird.ConvertTo<string>().Should().Be("");
+
+        // nullable
+        "11".ConvertTo<int?>().Should().Be(11);
+        bird.ConvertTo<int?>().Should().Be(null);
+        "abc".ConvertTo<int?>(0).Should().Be(0);
+
+        BirdZoo birdZoo = null;
+        birdZoo.ConvertTo<Bird>().Should().Be(null);
+
+        birdZoo = new BirdZoo();
+        birdZoo.ConvertTo<Bird>(default).Should().Be(null);
+        var func2 = () => birdZoo.ConvertTo<Bird>();
+        func2.Should().Throw<InvalidCastException>();
     }
 
     private class Bird
