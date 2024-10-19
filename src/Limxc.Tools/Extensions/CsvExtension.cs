c6b2783a8a003608f33431448e2e6ac583f555ed
袁文一 <limxc@163.com>
2024-10-19 23:31:42 +0800

    增加基于Tuple的CSV, string转换. ToCsv(), CsvTo()

---
+++ src/Limxc.Tools/Extensions/CsvExtension.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Limxc.Tools.Extensions
+{
+    public static class CsvExtension
+    {
+        public static T[] CsvTo<T>(this string fileString, bool hasHeader = true, char sep = ',')
+        {
+            if (!IsTuple(typeof(T)))
+                throw new Exception("T must be Tuple");
+
+            var rst = new List<T>();
+
+            var fieldTypes = typeof(T)
+                .GetFields(BindingFlags.Instance | BindingFlags.Public)
+                .Select(p => p.FieldType)
+                .ToArray();
+
+            var allowedTypes = AllowedTypes();
+            if (fieldTypes.Any(p => !allowedTypes.Contains(p)))
+                throw new Exception("T is nested Tuple");
+
+            var typeCount = fieldTypes.Length;
+
+            var lines = fileString.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var line in lines.Skip(hasHeader ? 1 : 0))
+                try
+                {
+                    if (string.IsNullOrWhiteSpace(line))
+                        continue;
+                    var row = line.Split(sep);
+                    var pars = new object[typeCount];
+                    for (var i = 0; i < typeCount; i++) pars[i] = Convert.ChangeType(row[i], fieldTypes[i]);
+                    rst.Add((T)Activator.CreateInstance(typeof(T), pars));
+                }
+                catch (Exception ex)
+                {
+                    throw new InvalidDataException($"Parse Error({ex.Message}): {line}");
+                }
+
+            return rst.ToArray();
+        }
+
+        public static string ToCsv<T>(this T[] source, string[] header = null, string sep = ",")
+        {
+            if (!IsTuple(typeof(T)))
+                throw new Exception("T must be Tuple");
+
+            var sb = new StringBuilder();
+
+            var fieldInfos = typeof(T)
+                .GetFields(BindingFlags.Instance | BindingFlags.Public)
+                .OrderBy(p => p.Name)
+                .ToArray();
+
+            var allowedTypes = AllowedTypes();
+            if (fieldInfos.Any(p => !allowedTypes.Contains(p.FieldType)))
+                throw new Exception("T is nested Tuple");
+
+            if (header?.Length > 0) sb.AppendLine(string.Join(sep, header));
+
+            for (var i = 0; i < source.Length; i++)
+            {
+                var obj = source[i];
+                sb.AppendLine(string.Join(sep, fieldInfos.Select(p => Format(p.GetValue(obj)))));
+            }
+
+            return sb.ToString();
+        }
+
+        private static Type[] AllowedTypes()
+        {
+            return new[]
+            {
+                typeof(int),
+                typeof(long),
+                typeof(float),
+                typeof(double),
+                typeof(decimal),
+                typeof(string),
+                typeof(bool)
+            };
+        }
+
+        private static bool IsTuple(Type type)
+        {
+            return type.IsGenericType &&
+                   (
+                       type.GetGenericTypeDefinition() == typeof(Tuple<>) ||
+                       type.GetGenericTypeDefinition() == typeof(ValueTuple<>) ||
+                       (type.GetGenericTypeDefinition().FullName?.StartsWith("System.Tuple`") ?? false) ||
+                       (type.GetGenericTypeDefinition().FullName?.StartsWith("System.ValueTuple`") ?? false)
+                   );
+        }
+
+        private static string Format(object value)
+        {
+            switch (value)
+            {
+                case null:
+                    return "";
+                case DateTime dateTime:
+                    return dateTime.ToString("yyyy-MM-dd HH:mm:ss");
+                case DateTimeOffset dateTimeOffset:
+                    return dateTimeOffset.ToString("yyyy-MM-dd HH:mm:ss");
+            }
+
+            if (value.GetType().FullName == "System.DateOnly")
+                return ((IFormattable)value).ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
+
+            var text = (value.ToString() ?? "").Replace("\"", "\"\"");
+            if (text.Length > 0 &&
+                (text.Contains(",") || text.Contains("\"") || text.Any(c => c < ' ') || char.IsWhiteSpace(text[0]) ||
+                 // ReSharper disable once UseIndexFromEndExpression
+                 char.IsWhiteSpace(text[text.Length - 1]))
+               )
+                text = "\"" + text + "\"";
+
+            return text;
+        }
+    }
+}
---
+++ tests/Limxc.ToolsTests/Extensions/CsvExtensionTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using FluentAssertions;
+using Limxc.Tools.Extensions;
+using Xunit;
+
+namespace Limxc.ToolsTests.Extensions;
+
+public class CsvExtensionTests
+{
+    [Fact]
+    public void CsvTest()
+    {
+        var data = new[]
+        {
+            (1, 3.3, "1-10岁"),
+            (10, 9.6, "10以上岁")
+        };
+
+        var header = new[] { "Age", "Par", "Desc" };
+
+        var csvStr = data.ToCsv(header);
+        var csvData = csvStr.CsvTo<(int, double, string)>();
+
+        data.Should().BeEquivalentTo(csvData);
+        csvStr.Should().Contain(string.Join(",", header));
+
+        Action act1 = () => new[]
+        {
+            (1, 3.3, (3f, true), "1-10岁"),
+            (10, 9.6, (9f, false), "10以上岁")
+        }.ToCsv();
+        act1.Should().Throw<Exception>();
+
+
+        Action act2 = () => new[]
+        {
+            (1, 3.3, new A(), "1-10岁"),
+            (10, 9.6, new A(), "10以上岁")
+        }.ToCsv();
+        act2.Should().Throw<Exception>();
+    }
+
+    private class A
+    {
+    }
+}
